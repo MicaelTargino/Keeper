4281e1b239635c74939e04fe661086e7f1dc4d5a
Micael Gomes Targino <micaeltargino@Luizs-MacBook-Pro.local>
2023-04-29 00:18:49 -0300

    made the note a former out component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@ import { useState } from 'react'
 import Header from '../components/Header.jsx'
 import Note from '../components/Note.jsx'
 import Footer from '../components/Footer.jsx'
+import notes from './data/notes.js'
+
+function createNote(note) {
+  return (
+    <Note key={note.key} title={note.title} content={note.content} />
+  )
+}
+
+
 function App() {
 
   return (
     <>
       <Header />
 
-      <Note />
+      {notes.map(createNote)}      
      
       <Footer />
     </>
